7b6227e7bd8e4f21579ce6d23b38b5663c5c9e23
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-03-15 20:23:38 +0100

    Add files via upload

---
+++ ilosc_krawedzi_wielokata_dwa_wzory.py
@@ -0,0 +1,8 @@
+for n in range(10):
+    # n = input("Podaj ilosc wierzcholkow wielokata: ")
+    k = (n*(n-3))/2 + n
+    h = (n*(n-1))/2
+    print n
+    print "Ilosc krawedzi wynosi: ", k
+    print "Ilosc krawedzi wynosi: ", h
+    print

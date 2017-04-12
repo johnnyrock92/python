d98a31cc00704069dc66a5cf8b4441e94f1f4a39
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-04-12 20:53:12 +0200

    poprawki

--- pole.py
+++ pole.py
@@ -2,15 +2,18 @@ print "Program obliczajacy pole"
 print "\nWybierz figure"
 print "1. Prostokat"
 print "2. Kolo"
-
+print "3. Kwadrat"
 figura = input("> ")
 
 if figura == 1:
-    wysokosc = input("Wpisz wysokosc prostakata: ")
-    szerokosc = input("Wpisz szerokosc prostokata: ")
-    pole = wysokosc * szerokosc
-else:
-    r = input("Wpisz promien: ")
+    a = input("Podaj a: ")
+    b = input("Podaj b: ")
+    pole = a * b
+elif figura == 2:
+    r = input("Podaj r: ")
     pole = 3.14*(r**2)
+else:
+    a = input("Podaj a: ")
+    pole = a**2
 
 print "Pole wynosi %d" % pole
--- pole_lambda.py
+++ pole_lambda.py
@@ -1,18 +1,22 @@
 print "Program obliczajacy pole WERSJA LAMBDA"
-
-print "Wybierz figure"
+print "\nWybierz figure"
 print "1. Prostokat"
 print "2. Kolo"
-
+print "3. Kwadrat"
 figura = input("> ")
+
+pole_prostokata = lambda a, b: a*b
+pole_kola = lambda pi, r: pi*r**2
+pole_kwadratu = lambda a: a**2
 
 if figura == 1:
     a = input("Podaj a: ")
     b = input("Podaj b: ")
-    pole = lambda a, b: a*b
-    print pole(a, b)
-else:
+    print pole_prostokata(a, b)
+elif figura == 2:
     r = input("Podaj r: ")
     pi = 3.14
-    pole = lambda pi, r: pi*r**2
-    print pole(pi, r)
+    print pole_kola(pi, r)
+else:
+    a = input("Podaj a: ")
+    print pole_kwadratu(a)

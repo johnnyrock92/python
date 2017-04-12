799a86c181b0344e22bc64a6906eb1e24606bd17
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-04-12 15:10:13 +0200

    przeniesienie do nowego katalogu

--- ilosc_krawedzi_wielokata.py
+++
@@ -1,3 +0,0 @@
-n = input("Podaj ilosc wierzcholkow wielokata: ")
-k = (n*(n-3))/2 + n
-print "Ilosc krawedzi wynosi: ", k
---
+++ mini_code/ilosc_krawedzi_wielokata.py
@@ -0,0 +1,3 @@
+n = input("Podaj ilosc wierzcholkow wielokata: ")
+k = (n*(n-3))/2 + n
+print "Ilosc krawedzi wynosi: ", k

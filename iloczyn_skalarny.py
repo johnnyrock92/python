3c7e356394cf55057717e58f98b95f08840b469b
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-04-13 22:23:00 +0200

    wymiana na for xrange

--- iloczyn_skalarny.py
+++ iloczyn_skalarny.py
@@ -2,9 +2,7 @@
 wektor1 = [1,2,3]
 wektor2 = [4,5,6]
 wynik = []
-i=0
-while i < len(wektor1):
+for i in xrange(len(wektor1)):
     k = wektor1[i] * wektor2[i]
     wynik.append(k)
-    i+=1
 print wynik

4dafb6791f8da2439cca930567dd75a6c3833212
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-04-13 19:18:35 +0200

    slowny zapis liczby 13042017 19:18

---
+++ slowny_zapis_liczby.py
@@ -0,0 +1,18 @@
+slownik = {0:'zero',
+           1:'jeden',
+           2:'dwa',
+           3:'trzy',
+           4:'cztery',
+           5:'piec',
+           6:'szesc',
+           7:'siedem',
+           8:'osiem',
+           9:'dziewiec'}
+
+def konwerter(liczba):
+    liczba_rozdzielona = map(int,str(liczba))
+    for i in liczba_rozdzielona:
+        print slownik[i],
+
+liczba = input('Podaj dowolna liczbe: ')
+konwerter(liczba)

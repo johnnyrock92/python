2fcf0b7873e1480fd471b55d81d2dc2d1706e3bf
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-04-13 22:01:48 +0200

    wymiana na for xrange

--- ilosc_dni.py
+++ ilosc_dni.py
@@ -2,7 +2,7 @@
 miesiace = {1:0,2:31,3:59,4:90,5:120,6:151,7:181,8:212,9:243,10:273,11:304,12:334}
 miesiace_p = {1:0,2:31,3:60,4:91,5:121,6:152,7:182,8:213,9:244,10:274,11:305,12:335}
 
-data1 = [13,4,2017]
+data1 = [13,4,2018]
 data2 = [1,1,2017]
 ilosc = []
 lista = []
@@ -25,11 +25,9 @@ def roznica_dat(data1, data2):
             wynik = miesiac1 - miesiac2
             ilosc.append(wynik)
         else:
-            while data1[i] > data2[i]:
-                lista.append(data2[i])
-                data2[i]+=1
+            for x in xrange(data2[i],data1[i]):
+                lista.append(x)
             #print lista
-
             for a in lista:
                 if (a % 4 == 0 and a % 100 != 0) or a % 400 == 0:
                     dni = 366

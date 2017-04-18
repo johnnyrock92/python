7418b2724db961725df32cda45a2ead975f5d05c
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-04-18 22:20:51 +0200

    usuniecie znaku komentarza

---
+++ lambda_sortowanie_ver2.py
@@ -0,0 +1,29 @@
+osoby = [{'nazwisko': 'Nowak', 'waga': 70, 'ulubione_dania': ['schabowy', 'pomidorowa']},
+         {'nazwisko': 'Kos',   'waga': 30, 'ulubione_dania': ['lody', 'ciastka']},
+         {'nazwisko': 'Szpak', 'waga': 40, 'ulubione_dania': ['ryz', 'ogorkowa']},
+         {'nazwisko': 'Kowalski', 'waga': 70, 'ulubione_dania': ['schabowy', 'pomidorowa']},
+         {'nazwisko': 'Konstantynopolitanczykiewicz', 'waga': 80, 'ulubione_dania': ['frytki', 'ryba']},
+         ]
+
+# Sortowanie wg nazwisk---------------------------------------------
+wg_nazwisko = [nazwa['nazwisko'] for nazwa in sorted(osoby, key=lambda o: o['nazwisko'])]
+print wg_nazwisko                 # ['Konstantynopolitanczykiewicz', 'Kos', 'Kowalski', 'Nowak', 'Szpak']
+
+wg_nazwisko.sort(reverse=True)
+print wg_nazwisko                 # ['Szpak', 'Nowak', 'Kowalski', 'Kos', 'Konstantynopolitanczykiewicz']
+
+
+# Sortowanie wg wagi------------------------------------------------
+wg_waga = [waga['waga'] for waga in sorted(osoby, key=lambda o: o['nazwisko'])]
+print sorted(wg_waga)             # [30, 40, 70, 70, 80]
+
+wg_waga.sort(reverse=True)
+print wg_waga                     # [80, 70, 70, 40, 30]
+
+# Sortowanie wg pierwszego dania------------------------------------------------
+
+#wg_dania = [danie['ulubione_dania'][0] for danie in sorted(osoby, cmp=lambda x, y: 1 if x < y else -1, key=lambda o: o['ulubione_dania'][0])]
+print sorted(wg_dania)
+
+wg_dania.sort(reverse=True)
+print wg_dania

e8e1dda561cdf8d656f93ae08c9897014326d2de
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-04-18 22:22:24 +0200

    usuniecie znaku komentarza

--- lambda_sortowanie_ver1.py
+++ lambda_sortowanie_ver1.py
@@ -22,8 +22,8 @@ print wg_waga                     # [80, 70, 70, 40, 30]
 
 # Sortowanie wg pierwszego dania------------------------------------------------
 
-#wg_dania = [danie['ulubione_dania'][0] for danie in sorted(osoby, cmp=lambda x, y: 1 if x < y else -1, key=lambda o: o['ulubione_dania'][0])]
-print sorted(wg_dania)
+wg_dania = [danie['ulubione_dania'][0] for danie in sorted(osoby, cmp=lambda x, y: 1 if x < y else -1, key=lambda o: o['ulubione_dania'][0])]
+print sorted(wg_dania)            # ['frytki', 'lody', 'ryz', 'schabowy', 'schabowy']
 
 wg_dania.sort(reverse=True)
-print wg_dania
+print wg_dania                    # ['schabowy', 'schabowy', 'ryz', 'lody', 'frytki']

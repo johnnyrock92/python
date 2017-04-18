bf90bb596d2d88c0df8a2cd5a0f1342c23cc105a
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-04-18 15:00:03 +0200

    Porownanie lambdy i funkcji

---
+++ lambda_vs_funkcja.py
@@ -0,0 +1,13 @@
+# Porownanie lambdy i funkcji
+x = 2
+y = 3
+
+#Lambda-----------------------
+test = lambda a, b: a*b
+print test(x, y)
+
+#Funkcja----------------------
+def funkcja(a,b):
+    print a*b
+
+funkcja(x,y)

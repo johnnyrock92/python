32b9a7e232af2c0f3cb94a14cb018ec3074e499d
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-04-18 22:37:12 +0200

    sortowanie calej listy

--- lambda_sortowanie_ver2.py
+++ lambda_sortowanie_ver2.py
@@ -6,24 +6,20 @@ osoby = [{'nazwisko': 'Nowak', 'waga': 70, 'ulubione_dania': ['schabowy', 'pomid
          ]
 
 # Sortowanie wg nazwisk---------------------------------------------
-wg_nazwisko = [nazwa['nazwisko'] for nazwa in sorted(osoby, key=lambda o: o['nazwisko'])]
-print wg_nazwisko                 # ['Konstantynopolitanczykiewicz', 'Kos', 'Kowalski', 'Nowak', 'Szpak']
+print sorted(osoby, key=lambda o: o['nazwisko'])
+print '---------------------------------------------------------------'
+print sorted(osoby, key=lambda o: o['nazwisko'], reverse=True)
 
-wg_nazwisko.sort(reverse=True)
-print wg_nazwisko                 # ['Szpak', 'Nowak', 'Kowalski', 'Kos', 'Konstantynopolitanczykiewicz']
-
+print '\n\n'
 
 # Sortowanie wg wagi------------------------------------------------
-wg_waga = [waga['waga'] for waga in sorted(osoby, key=lambda o: o['nazwisko'])]
-print sorted(wg_waga)             # [30, 40, 70, 70, 80]
+print sorted(osoby, key=lambda o: o['waga'])
+print '---------------------------------------------------------------'
+print sorted(osoby, key=lambda o: o['waga'], reverse=True)
 
-wg_waga.sort(reverse=True)
-print wg_waga                     # [80, 70, 70, 40, 30]
+print '\n\n'
 
 # Sortowanie wg pierwszego dania------------------------------------------------
-
-#wg_dania = [danie['ulubione_dania'][0] for danie in sorted(osoby, cmp=lambda x, y: 1 if x < y else -1, key=lambda o: o['ulubione_dania'][0])]
-print sorted(wg_dania)
-
-wg_dania.sort(reverse=True)
-print wg_dania
+print sorted(osoby, cmp=lambda x, y: 1 if x > y else -1, key=lambda o: o['ulubione_dania'][0])
+print '---------------------------------------------------------------'
+print sorted(osoby, cmp=lambda x, y: 1 if x < y else -1, key=lambda o: o['ulubione_dania'][0])

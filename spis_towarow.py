f205e9f11017e200203cfad08bc7606667362ec1
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-04-18 12:04:26 +0200

    poprawka w argumentach funkcji

--- spis_towarow.py
+++ spis_towarow.py
@@ -1,14 +1,14 @@
-slownik = {'chleb':2,
-           'cukier':3,
-           'maka':1.5,
-           'sol':1,
-           'ziemniaki':1.7
-          }
+slownik_produktow = {'chleb':2,
+                     'cukier':3,
+                     'maka':1.5,
+                     'sol':1,
+                     'ziemniaki':1.7
+                     }
 
 lista_towarow = ['chleb', 'cukier', 'maka', 'sol']
 
-def spis_towarow(spis):
+def spis_towarow(slownik, spis):
     for i in spis:
         print "%s: %dzl" % (i, slownik[i])
 
-spis_towarow(lista_towarow)
+spis_towarow(slownik_produktow, lista_towarow)

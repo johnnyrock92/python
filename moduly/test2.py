29dcc6cdcaa152c0f9ab370bd1720aff5d21756e
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-04-21 16:52:25 +0200

    Delete test2.py

--- moduly/test2.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sys.path.append(r'E:\Janusz\Documents\Dropbox\Studia\semestr_2\python\python\moduly\lib')
-
-from modul import *
-
-print add(2,3)            # 5
-print subtraction(10,6)   # 4
-print power(5,2)          # 25
